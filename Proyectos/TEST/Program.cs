240ade2c352d5d6f7e247fbd051a2046cc11c9cc
aamores <aamores@mafroda.es>
2023-07-10 14:54:43 +0200

    V1.0.2

--- Proyectos/TEST/Program.cs
+++ Proyectos/TEST/Program.cs
@@ -5,7 +5,7 @@ namespace TEST {
   class Program {
 
     static void Main(string[] args) {
-      //V1.0
+      //V1.0.2
       // El comando de PowerShell que quieres ejecutar
       string command = "Get-Process";
 
